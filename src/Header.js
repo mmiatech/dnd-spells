79a67b6a03677c00d170d95f1018d5c91a441e17
mmiatech <mmiatech@madisoncollege.edu>
2021-11-03 16:43:12 -0500

    cleanup, header component created

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="App-header">
+            <h1>D&D 5th Edition Spell Search</h1>
+        </header>
+    );
+  }
+
+export default Header;
