609478389feb469b2e41e872aa7c0e22bff999ca
mmiatech <mmiatech@madisoncollege.edu>
2021-12-02 19:04:10 -0600

    some info display styling

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Form from './Form';
 import Error from './Error';
 //import Loader from './Loader';
 import SpellInfo from './SpellInfo';
+import Container from '@mui/material/Container';
 
 import useSpellName from './hooks/useSpellName';
+import { Paper } from '@mui/material';
 
 const App = () => {
     const { isError, isLoading, spellInfo, submitRequest } = useSpellName();
@@ -17,14 +19,21 @@ const App = () => {
     return (
         <>
             <Header />
-            {!spellInfo && (
-                <div class="info-display">
-                    {isError && <Error searchTerm={isError}/>}
-                    {!isLoading && <Form submitSearch={onSubmit}/>}
-                    {/* {isLoading && <Loader />} */}
-                </div>
-            )}
-            {spellInfo && <SpellInfo spellInfo={spellInfo} />}
+            <Container
+                maxWidth="md"
+                class="info-display-container"
+            >
+                <Paper elevation={3} class="info-display-inner">
+                    {!spellInfo && (
+                        <div>
+                            {isError && <Error searchTerm={isError}/>}
+                            {!isLoading && <Form submitSearch={onSubmit}/>}
+                            {/* {isLoading && <Loader />} */}
+                        </div>
+                    )}
+                    {spellInfo && <SpellInfo spellInfo={spellInfo} />}
+                </Paper>
+            </Container>
         </>
     );
 }
