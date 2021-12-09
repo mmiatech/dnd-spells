995ba250392232717c55e460e11f9f906dfa99b7
mmiatech <mmiatech@madisoncollege.edu>
2021-12-08 18:12:42 -0600

    another typo

--- src/hooks/useSpellName.js
+++ src/hooks/useSpellName.js
@@ -24,7 +24,6 @@ const useSpellName = () => {
             const {data} = await axios(`${baseUrl}/api/spells/${spellName}`);
             setSpellInfo({data});
         } catch (err) {
-            spellName = spellName.replaceAll('-', ' ');
             setError(spellName);
             setLoading(false);
             return;
